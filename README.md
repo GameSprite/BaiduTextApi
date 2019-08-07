1b6aad2401226719e5e57a467e46da3f83c76ffd
陈冠州 <1668831812@qq.com>
2019-08-07 17:43:39 +0800

    finish

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 rewrite with nodejs.
 
 origin project url: https://github.com/chenguanyou/BaiduTextApi.git (python3 version)
+
+经过测试发现使用高精度含位置识别的接口，汉字可以识别出来，但是汉字的位置信息误差较大。
--- index.js
+++ index.js
@@ -1,30 +1,118 @@
 /****************************************************
  * BaiduTextApi 的封装
- * Edit by : ChenGuanzhou (chenguanzhou@huohua.cn)
+ * Edit by : ChenGuanzhou (1668831812@qq.com)
  * Create at : 2019-08-06 20:18:02
  *****************************************************/
 var request = require("request");
 var config = require("./config");
+var fs = require("fs");
+var path = require("path");
+var urlencode = require("urlencode");
 
 //get access token
 function getAccessToken() {
     return new Promise(function(resolve, reject) {
         request.post(config.URL_CONFIG.ACCESS_TOKEN_URL, {}, (error, response) => {
             if (!error && response.statusCode == 200) {
-                resolve(response.toJSON());
-            } else if (error == "invalid_client") {
-                reject("获取accesstoken错误，请检查API_KEY，SECRET_KEY是否正确！");
+                resolve(JSON.parse(response.body).access_token);
+            } else {
+                if (error) {
+                    reject(error.toLocaleString());
+                } else {
+                    reject(response.body);
+                }
             }
         });
     });
 }
 
+//get url of Accurate api
+function getAccurateUrl(accessToken) {
+    return config.URL_CONFIG["ACCURATE"] + "?access_token=" + accessToken;
+}
+
+function getPostRequestData(
+    imagePath = "",
+    recognize_granularity = "small",
+    detect_direction = true,
+    vertexes_location = false,
+    probability = true
+) {
+    if (!imagePath) {
+        console.error("imagePath参数不能为空");
+        return;
+    }
+    if (!path.isAbsolute(imagePath)) {
+        console.error("" + imagePath + "不是绝对路径");
+    }
+
+    let data = {
+        image: null,
+        recognize_granularity,
+        detect_direction,
+        vertexes_location,
+        probability
+    };
+
+    //读图片的数据并base64加密
+    try {
+        data.image = urlencode(fs.readFileSync(imagePath).toString("base64"));
+    } catch (e) {
+        console.error(e.toLocaleString());
+        return;
+    }
+
+    return data;
+}
+function obj2FormUrl(obj) {
+    let r = "";
+    for (const k in obj) {
+        if (r) {
+            r += "&";
+        }
+        if (obj.hasOwnProperty(k)) {
+            r += `${k}=${obj[k]}`;
+        }
+    }
+    return r;
+}
+
+function getImageTextData(accessToken, requestData) {
+    return new Promise(function(resolve, reject) {
+        request(
+            {
+                url: getAccurateUrl(accessToken),
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: obj2FormUrl(requestData)
+            },
+            (error, response) => {
+                if (!error && response.statusCode == 200) {
+                    resolve(response.body);
+                } else {
+                    if (error) {
+                        reject(error.toLocaleString());
+                    } else {
+                        reject(response.body);
+                    }
+                }
+            }
+        );
+    });
+}
+
 async function exec() {
     try {
-        var accessToken = await getAccessToken();
-        console.log(accessToken);
+        let accessToken = await getAccessToken();
+        let imagePath = process.argv.splice(2)[0];
+        let requestData = getPostRequestData(imagePath);
+        if (!requestData) return;
+        let imageTextData = await getImageTextData(accessToken, requestData);
+        console.log(imageTextData);
     } catch (e) {
-        console.log(e);
+        console.error("Exception: ", e);
     }
 }
 
